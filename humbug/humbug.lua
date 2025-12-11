9a4605cc62ad42359172da13b6caaa87320077a2
Jeff <phoenik@gmail.com>
2025-12-11 12:47:33 -0800

    Update humbug.lua

--- humbug/humbug.lua
+++ humbug/humbug.lua
@@ -20,4 +20,12 @@ ashita.events.register('packet_in', 'packet_in_cb', function (e)
             ashita.bits.pack_be(e.data_modified_raw, 110, 88, 0, 16);
         end
     end
+
+    if (e.id == 0x5F) then
+        local song = struct.unpack('H', e.data, 7); -- Incoming Song
+
+        if (song == 0xEF) then
+            e.blocked = true;
+        end
+    end
 end);
